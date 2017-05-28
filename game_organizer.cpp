43ba81de6f2bf1e7bb261e296209526d3c79acc8
Celestial Phineas <hanghang0713@hotmail.com>
2017-05-28 21:08:52 +0800

    Refactor the game organizer, decouple the game organizer and the board, winning judge and the player. Only pointers are used to aggregate.

---
+++ game_organizer.cpp
@@ -0,0 +1,41 @@
+#include "game_organizer.h"
+
+GameOrganizer::GameOrganizer(const GameOrganizer &src)
+{
+    board = src.board;
+    judge = src.judge;
+    black_player = src.black_player;
+    white_player = src.white_player;
+    return;
+}
+
+GameOrganizer::GameOrganizer(Board *_board, WinningJudge *_judge, Player *_black, Player *_white)
+{
+    board = _board; judge = _judge;
+    black_player = _black; white_player = _white;
+    return;
+}
+
+void GameOrganizer::exchange_player()
+{
+    Player *temp = black_player;
+    black_player = white_player;
+    white_player = temp;
+    if(black_player) black_player->set_stone(Player::black);
+    if(white_player) white_player->set_stone(Player::white);
+    return;
+}
+
+void GameOrganizer::set_black(Player *player)
+{
+    black_player = player;
+    if(player) black_player->set_stone(Player::black);
+    return;
+}
+
+void GameOrganizer::set_white(Player *player)
+{
+    white_player = player;
+    if(player) white_player->set_stone(Player::white);
+    return;
+}
---
+++ game_organizer.h
@@ -0,0 +1,35 @@
+// Game class interface
+#ifndef __GAME_ORGANIZER_H_
+#define __GAME_ORGANIZER_H_
+
+#include "board.h"
+#include "player.h"
+#include "free_style_judge.h"
+
+// Game organizer abstract interface
+class GameOrganizer
+{
+public:
+    GameOrganizer(const GameOrganizer&);
+    GameOrganizer(Board *_board = NULL, WinningJudge *_judge = NULL,
+        Player *_black = NULL, Player *_white = NULL);
+    virtual ~GameOrganizer() {return;}
+    // The exchange_player method exchanges the pointers "black_player" and 
+    // "white_player", if no NULL occurs, the method also let the players change
+    // the stones that they are playing.
+    void exchange_player();
+    void set_board(Board *_board) {board = _board;}
+    void set_judge(WinningJudge *_judge) {judge = _judge;}
+    void set_black(Player*);
+    void set_white(Player*);
+    virtual void request_te() = 0;
+protected:
+    // A game instance has its own components!
+    // The pointers below each point to an unique instance.
+    Board *board;
+    WinningJudge *judge;
+    Player *black_player;
+    Player *white_player;
+};
+
+#endif
