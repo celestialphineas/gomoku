3457231c012409a7a1df3f314ccffeb47bc6c3aa
Celestial Phineas <hanghang0713@hotmail.com>
2017-05-28 21:46:36 +0800

    Initial commit continued with some comments added.

--- single_game_factory.h
+++ single_game_factory.h
@@ -1,3 +1,6 @@
+// Header file of single game factory.
+// No factory pattern is here, just a simple factory used to generate each
+// single game.
 #ifndef __SINGLE_GAME_FACTORY_H_
 #define __SINGLE_GAME_FACTORY_H_
 
