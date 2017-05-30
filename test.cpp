18c5a07669539395ac753c7cdfca7975f4b70618
Celestial Phineas <hanghang0713@hotmail.com>
2017-05-30 20:20:09 +0800

    Straight finder for rows.

--- test.cpp
+++ test.cpp
@@ -19,29 +19,29 @@ void print_board(const Board& board)
     return;
 }
 
-// void print_threats(const std::vector<Threat> &src)
-// {
-//     if(src.empty()) puts("No threats is found.");
-//     else
-//     {
-//         for(std::vector<Threat>::const_iterator i = src.begin();
-//             i != src.end(); i++)
-//         {
-//             std::cout << "Threat from "
-//                 << (i->threat_src ? "white" : "black") << ":" << std::endl;
-//             std::cout << i->pos1_x << " " << i->pos1_y << " "
-//                 << i->pos2_x << " " << i->pos2_y << std::endl;
-//             std::cout << "Key positions: ";
-//             for(std::vector<std::vector<unsigned> >::const_iterator
-//                 j = i->key_pos_list.begin(); j != i->key_pos_list.end(); j++)
-//             {
-//                 std::cout << (*j)[0] << " " << (*j)[1] << "  ";
-//             }
-//             std::cout << std::endl;
-//         }
-//     }
-//     return;
-// }
+void print_threats(const std::vector<ThreatFinder::Threat> &src)
+{
+    if(src.empty()) puts("No threats is found.");
+    else
+    {
+        for(std::vector<ThreatFinder::Threat>::const_iterator i = src.begin();
+            i != src.end(); i++)
+        {
+            std::cout << "Threat from "
+                << (i->threat_src ? "white" : "black") << ":" << std::endl;
+            std::cout << i->pos1_x << " " << i->pos1_y << " "
+                << i->pos2_x << " " << i->pos2_y << std::endl;
+            std::cout << "Key positions: ";
+            for(std::vector<std::vector<unsigned> >::const_iterator
+                j = i->key_pos_list.begin(); j != i->key_pos_list.end(); j++)
+            {
+                std::cout << (*j)[0] << " " << (*j)[1] << "  ";
+            }
+            std::cout << std::endl;
+        }
+    }
+    return;
+}
 
 void print_rows(const std::vector<Board::SelectedRow> &rows)
 {
@@ -68,12 +68,12 @@ int main(void)
     {
         system("cls");
         print_board(*board);
-        // ThreatFinder threat_finder(board);
-        // std::vector<Threat> black_threats
-        //     = threat_finder.find_one_end_blocked(ThreatFinder::black, 3);
-        // std::vector<Threat> white_threats
-        //     = threat_finder.find_one_end_blocked(ThreatFinder::white, 3);
-        // print_threats(black_threats); print_threats(white_threats);
+        ThreatFinder threat_finder(board);
+        std::vector<ThreatFinder::Threat> black_threats
+            = threat_finder.find_straight(ThreatFinder::black, 3);
+        std::vector<ThreatFinder::Threat> white_threats
+            = threat_finder.find_straight(ThreatFinder::white, 3);
+        print_threats(black_threats); print_threats(white_threats);
         std::cout << "Operation sequence: ";
         std::deque<Te> sequence = board->get_game_sequence();
         for(std::deque<Te>::iterator i = sequence.begin();
--- threat_finder.cpp
+++ threat_finder.cpp
@@ -1,19 +1,7 @@
 #include "threat_finder.h"
 
-// std::vector<ThreatFinder::Threat> ThreatFinder::find_straight(
-//     bool who, unsigned n_to_check) const
-// {
-//     // Check the rows
-//     std::vector<Board::SelectedRow> rows = board->get_rows();
-//     for(std::vector<Board::SelectedRow>::const_iterator row = rows.begin();
-//         row != rows.end(); row++)
-//     {
-
-//     }
-// }
-
 // An implementation of KMP o(TヘTo)~~~  
-std::vector<unsigned> find_occurance(
+std::vector<unsigned> find_occurances(
     const std::vector<Board::PosStatus> &v, unsigned arr[], unsigned len)
 {
     std::vector<unsigned> *result = new std::vector<unsigned>;
@@ -50,3 +38,41 @@ std::vector<unsigned> find_occurance(
     delete[] funct;
     return *result;
 }
+
+std::vector<ThreatFinder::Threat> ThreatFinder::find_straight(
+    bool who, unsigned n_to_check) const
+{
+    std::vector<Threat> *result = new std::vector<Threat>;
+    unsigned *arr = new unsigned[n_to_check + 2];
+    for(unsigned i = 0; i < n_to_check + 2; i++)
+        arr[i] = (who ? Board::white_stone : Board::black_stone);
+    arr[0] = Board::accessible;
+    arr[n_to_check + 1] = Board::accessible;
+    // Check the rows
+    std::vector<Board::SelectedRow> rows = board->get_rows();
+    for(std::vector<Board::SelectedRow>::const_iterator row = rows.begin();
+        row != rows.end(); row++)
+    {
+        std::vector<unsigned> occurances
+            = find_occurances(row->row_list, arr, n_to_check + 1);
+        for(std::vector<unsigned>::iterator i = occurances.begin();
+            i != occurances.end(); i++)
+        {
+            Threat threat;
+            threat.threat_src = who;
+            threat.pos1_x = row->begin_x + *i + 1;
+            threat.pos1_y = row->begin_y;
+            threat.pos2_x = row->begin_x + *i + n_to_check;
+            threat.pos2_y = row->begin_y;
+            std::vector<unsigned> coord;
+            coord.push_back(row->begin_x + *i); coord.push_back(row->begin_y);
+            threat.key_pos_list.push_back(coord);
+            coord[0] = row->begin_x + *i + n_to_check - 1;
+            threat.key_pos_list.push_back(coord);
+            result->push_back(threat);
+        }
+    }
+    delete[] arr;
+    return *result;
+}
+
