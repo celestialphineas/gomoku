5309273101c584d6dbd37ba19792bd9dff4c3c60
Celestial Phineas <hanghang0713@hotmail.com>
2017-06-21 19:53:43 +0800

    Dialog.

---
+++ settings_dialog.cpp
@@ -0,0 +1,6 @@
+#include "settings_dialog.h"
+
+SettingsDialog::SettingsDialog(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ settings_dialog.h
@@ -0,0 +1,17 @@
+#ifndef SETTINGS_DIALOG_H
+#define SETTINGS_DIALOG_H
+
+#include <QWidget>
+
+class SettingsDialog : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SettingsDialog(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // SETTINGS_DIALOG_H
