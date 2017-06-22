280a92540eef34d1e7ac8ea3a95a4b6e33abbd21
lulucici <yolandachen7@zju.edu.cn>
2017-06-23 05:10:03 +0800

    Annotation for mainwindow

--- mainwindow.h
+++ mainwindow.h
@@ -10,45 +10,48 @@
 #include "game.h"
 #include "game_factory.h"
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow: public QMainWindow {
+ Q_OBJECT
 
-public:
-    explicit MainWindow(Game *game_, QWidget *parent = 0);
-    void set_game(Game *game_) {game = game_;}
-    void set_dialog(QWidget *widget_) {dialog = widget_;}
-    Game *get_game() {return game;}
-    ~MainWindow();
-private:
-    void paintEvent(QPaintEvent *event);
-    void mousePressEvent(QMouseEvent *event);
-    unsigned grid_size;
-    Game *game;
+ public:
+  explicit MainWindow(Game *game_, QWidget *parent = 0);
+  void set_game(Game *game_) { game = game_; }
+  void set_dialog(QWidget *widget_) { dialog = widget_; }
+  Game *get_game() { return game; }
+  ~MainWindow();
+ protected:
+  //Use QPainter to draw the board
+  void paintEvent(QPaintEvent *event);
+  //Monitor mouse press event which decide the behavior of main window
+  void mousePressEvent(QMouseEvent *event);
+ private:
+  unsigned grid_size;
+  Game *game;
 
-    int pen_thin = 2, pen_thick = 4;
-    QColor board_color;
-    QColor line_color;
-    QColor black_stone_color;
-    QColor white_stone_color;
-    QColor invalid_but_color;
-    QColor valid_but_color;
-    QPen thin_pen;
-    QPen thick_pen;
-    QPen black_stone_pen;
-    QPen white_stone_pen;
-    QPen black_text_pen;
-    QPen white_text_pen;
-    QPen invalid_but_pen;
-    QPen valid_but_pen;
-    QBrush default_brush;
-    QBrush black_stone_brush;
-    QBrush white_stone_brush;
-    double stone_radius_ratio;
-    QFont fa;
-    QFont minor;
+  //Colors、pens and brush for different elements
+  int pen_thin = 2, pen_thick = 4;
+  QColor board_color;
+  QColor line_color;
+  QColor black_stone_color;
+  QColor white_stone_color;
+  QColor invalid_but_color;
+  QColor valid_but_color;
+  QPen thin_pen;
+  QPen thick_pen;
+  QPen black_stone_pen;
+  QPen white_stone_pen;
+  QPen black_text_pen;
+  QPen white_text_pen;
+  QPen invalid_but_pen;
+  QPen valid_but_pen;
+  QBrush default_brush;
+  QBrush black_stone_brush;
+  QBrush white_stone_brush;
+  double stone_radius_ratio;
+  QFont fa;
+  QFont minor;
 
-    QWidget *dialog;
+  QWidget *dialog;
 };
 
 #endif // MAINWINDOW_H
--- settings_dialog.cpp
+++ settings_dialog.cpp
@@ -1,7 +1,7 @@
 #include "settings_dialog.h"
 
 /**
- * Draw the dialog for game setting
+ * The dialog for game setting
  * @param main_window_
  * @param parent
  */
@@ -29,18 +29,16 @@ SettingsDialog::SettingsDialog(MainWindow *main_window_, QWidget *parent) :
     create_new_but(tr("创建新的游戏")),
     cancel_but(tr("取消")),
 
-    //Confirm whether the input of width and height is valid
     width_validator(GameFactory::min_board_dim_width_threshold,
                     GameFactory::max_board_dim_width_threshold),
     height_validator(GameFactory::min_board_dim_height_threshold,
-                     GameFactory::max_board_dim_height_threshold)
-
-{
+                     GameFactory::max_board_dim_height_threshold) {
   setWindowIcon(QIcon(":/icons/icon.ico"));
   main_window = main_window_;
   setFixedSize(450, 280);
   setLayout(&layout);
-  //Draw the group box for player settings
+
+  //The group for player settings
   player_setting_layout.addWidget(&human_vs_ai);
   human_vs_ai.setChecked(true);
   player_setting_layout.addWidget(&ai_vs_human);
@@ -48,7 +46,7 @@ SettingsDialog::SettingsDialog(MainWindow *main_window_, QWidget *parent) :
   player_setting_group.setLayout(&player_setting_layout);
   layout.addWidget(&player_setting_group, 0, 0);
 
-  //Draw the group box for rule settings
+  //The group for rule settings
   rule_layout.addWidget(&free_style);
   free_style.setChecked(true);
   rule_layout.addWidget(&std_gomoku);
@@ -56,7 +54,7 @@ SettingsDialog::SettingsDialog(MainWindow *main_window_, QWidget *parent) :
   rule_group.setLayout(&rule_layout);
   layout.addWidget(&rule_group, 0, 1);
 
-  //Draw the group box for board size settings
+  //The group for board size settings
   board_setting_layout.addWidget(&width_label);
   board_setting_layout.addWidget(&width_edit);
   width_edit.setText("15");
@@ -68,7 +66,7 @@ SettingsDialog::SettingsDialog(MainWindow *main_window_, QWidget *parent) :
   board_setting_group.setLayout(&board_setting_layout);
   layout.addWidget(&board_setting_group, 0, 2);
 
-  //Draw the group box for board size setting
+  //The group for board size setting
   round_setting_layout.addWidget(&round1);
   round1.setChecked(true);
   round_setting_layout.addWidget(&round5);
@@ -79,13 +77,13 @@ SettingsDialog::SettingsDialog(MainWindow *main_window_, QWidget *parent) :
   layout.addWidget(&create_new_but, 1, 2);
   layout.addWidget(&cancel_but, 2, 2);
 
-  // Signal slot connection
+  //Signal slot connection
   connect(&create_new_but, SIGNAL(clicked()), SLOT(create_game_object()));
   connect(&cancel_but, SIGNAL(clicked()), SLOT(toggle_this()));
 }
 
 /**
- * Create a game object according to the setting.
+ * Create a game object according to the setting
  */
 void SettingsDialog::create_game_object() {
   GameFactory::PlayerType player1_type;
@@ -96,7 +94,7 @@ void SettingsDialog::create_game_object() {
   unsigned to_set_height = height_edit.text().toUInt();
   unsigned to_set_n_round;
 
-  //player settings
+  //Player settings
   if (human_vs_ai.isChecked()) {
     player1_type = GameFactory::human_player;
     player2_type = GameFactory::primary_ai;
@@ -108,7 +106,7 @@ void SettingsDialog::create_game_object() {
     player2_type = GameFactory::human_player;
   }
 
-  //judge rule ettings
+  //Judge rule settings
   if (free_style.isChecked()) {
     game_judge = GameFactory::free_style_gomoku;
   } else if (std_gomoku.isChecked()) {
